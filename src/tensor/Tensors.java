7dbf2f48d42517386104f4559efeb2121a0f13d5
ehdnd_mac <taeungjang@naver.com>
2025-05-26 11:26:56 +0900

    구현: 46. 행렬은 특정 두 열의 위치를 맞교환할 수 있다. #9

--- src/tensor/Tensors.java
+++ src/tensor/Tensors.java
@@ -2,14 +2,14 @@ package tensor;
 
 import java.math.BigDecimal;
 
-/*
+/*TODO
  * 모든 메소드가 public static
  * 위의 디폴트 static 메소드를 구현하라고 한 스칼라, 벡터, 행렬의 연산 기능을 호출한 후 반환된 객체를 반환하는 메소드들
  * Tensor의 사용자는 Test.java로 상정
  * 같은 방식으로 사용될 메소드들을 Tensors 클래스에 추가 가능
  */
 
-/*
+/*TODO
  * 디폴드 메서드들
  * 스칼라의 연산
  * 전달받은 두 스칼라의 덧셈
@@ -24,16 +24,16 @@ import java.math.BigDecimal;
 
 public class Tensors {
 
-    // ===== 스칼라 연산 =====
+  // ===== 스칼라 연산 =====
 
-    // 두 스칼라의 덧셈 결과를 반환
-    public static Scalar add(Scalar a, Scalar b) {
-        return ScalarImpl.add(a, b);
-    }
+  // 두 스칼라의 덧셈 결과를 반환
+  public static Scalar add(Scalar a, Scalar b) {
+    return ScalarImpl.add(a, b);
+  }
 
-    // 두 스칼라의 곱셈 결과를 반환
-    public static Scalar multiply(Scalar a, Scalar b) {
-        return ScalarImpl.multiply(a, b);
-    }
+  // 두 스칼라의 곱셈 결과를 반환
+  public static Scalar multiply(Scalar a, Scalar b) {
+    return ScalarImpl.multiply(a, b);
+  }
 
 }
